02970dd16757206a880be8edb2eecb2eac20fa3b
Tal Ovadia <tal_o@optimove.com>
2019-05-16 18:51:35 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const url = require('url');
+
+(function() {
+    http.createServer((req, res) => {
+        console.log(req.url)
+        const { url } = req;
+
+        if (url === '/intense') {
+            //do some cpuintesnse action
+            intensetCPURunner(60000);
+        }
+    }).listen(80);
+}());
+
+function intensetCPURunner(ms) {
+    console.log('making cpu intenst action')
+    
+    var now = new Date().getTime();
+    var result = 0;
+    while(true) {
+        result += Math.random() * Math.random();
+        let date = new Date().getTime();
+        if (date > now +ms)
+            return date;
+    }
+    
+}
+
+module.exports = intensetCPURunner;
